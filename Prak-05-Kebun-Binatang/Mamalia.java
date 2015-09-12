b381e18eba1c1be9c3a018eb833257ca675946df
Rikiandri <rikiandrean81@gmail.com>
2015-09-12 17:03:08 +0700

    no message

---
+++ Prak-05-Kebun-Binatang/Mamalia.java
@@ -0,0 +1,16 @@
+
+/**
+ * Write a description of interface Mamalia here.
+ * 
+ * @author (your name) 
+ * @version (a version number or a date)
+ */
+public interface Mamalia implements Binatang {
+    /**
+     * An example of a method header - replace this comment with your own
+     * 
+     * @param  y    a sample parameter for a method
+     * @return        the result produced by sampleMethod 
+     */
+    int sampleMethod(int y);
+}
