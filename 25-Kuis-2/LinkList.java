321d6f0b101350699ac662768b0c0d5a53cc6d74
Rikiandri <rikiandrean81@gmail.com>
2015-12-14 00:01:13 +0700

    kuis 2

---
+++ 25-Kuis-2/LinkList.java
@@ -0,0 +1,34 @@
+import java.math.BigDecimal;
+/**
+ * Write a description of class LinkList here.
+ * 
+ * @author (your name) 
+ * @version (a version number or a date)
+ */
+public class LinkList
+{
+    
+    public LinkList() {
+        kepala=null;
+    }
+    
+    public Node kepala() {
+        return kepala;
+    }
+    
+    public void tambah(BigDecimal data){
+        Node nodeBaru = new Node(data);
+        
+        if (kepala == null)
+        kepala=nodeBaru;
+        else{
+            Node sekarang=kepala;
+            while (sekarang.berikut != null)
+            sekarang=sekarang.berikut;
+            sekarang.berikut=nodeBaru;
+            
+        }
+    }
+    
+    private Node kepala;
+}
---
+++ 25-Kuis-2/Node.java
@@ -0,0 +1,15 @@
+import java.math.BigDecimal;
+/**
+ * Write a description of class Node here.
+ * 
+ * @author (your name) 
+ * @version (a version number or a date)
+ */
+public class Node{
+ public BigDecimal data;
+ public Node berikut;
+
+ public Node(BigDecimal data){
+     this.data=data;
+}
+}
